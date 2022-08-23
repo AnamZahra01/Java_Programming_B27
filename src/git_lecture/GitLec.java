527b96d6612fd614a768d35f2618305e1fbd3798
Anam Zahra <zanam090@gmail.com>
2022-08-23 13:35:08 -0400

    NewFile

---
+++ src/git_lecture/GitLec.java
@@ -0,0 +1,79 @@
+package git_lecture;
+
+public class GitLec {
+    /*
+    GIT: Version Control System -> keeps track of all changing.
+    GitHub: online platform that hosts our remote repositories.
+    Local repo: folder on our computer that keeps track of changes
+    Remote repo: online folder that keeps track of changes, others can share their changes
+    -> we use GitHub to host
+
+    UnVersioned file: not being kept track of
+    Commit: Saving your changes to local Repo
+    Commit Message: What changes (we are saving, we made)
+    Author: who made the commit
+    Push: sending changes from local repo to remote repo
+    Remote: actual project (link)
+
+    ------------------------------------------------------------------
+    Steps To Follow:
+    1- Enable VCS: Enable Git -> creating local repo
+    2- Create .gitignore: contains files we don't want to keep track of.
+    3- Commit
+    4- Share project on GitHub (done once)
+            Sharing does these steps:
+            1) Creates Remote Repo
+            2) Links Remote repo to our project
+            3) Pushes changes so far
+
+    flow of commit and push (from Local to Remote)
+        create a new package
+        create a new file
+        commit
+        create another new file
+        commit
+        push all changes
+
+    flow of Pull (fetch) -> (Remote to Local)
+        Go to project (open src file) on GitHub
+        add file -> create new java file
+            name: loops.java
+        commit that file on bottom of page
+
+    fetch: gives us information about the commit (preview) without putting changes to our project
+    update: gets the new changes for that branch
+    pull: more functionality to control which branch you pull from or to
+
+    Branches:
+        1) Make new branch from Master
+        2) Make sure to check out new branch
+        3) Make new changes and push
+        4) go to gitHub and create pull request to merge our origin name branch and master branch (origin is remote)
+
+    checkout: switch the branch
+    Head: current branch you are looking at
+    ---------------------------------------------------------
+    Clone:  create copy of a remote repo to your local repo
+
+    To clone office hours:
+    1) go to gitHub
+    2) click on account from top right
+    3) settings -> developer settings
+
+    Token: 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
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     */
+}
