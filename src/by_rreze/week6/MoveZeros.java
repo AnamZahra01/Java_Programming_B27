2607b6b43cae280fb9d2037e21e7792518d0b4f9
Anam Zahra <zanam090@gmail.com>
2022-11-24 05:53:21 -0500

    New File

--- src/by_rreze/week6/MoveZeros.java
+++ src/by_rreze/week6/MoveZeros.java
@@ -1,7 +1,46 @@
 package by_rreze.week6;
 
+import java.util.Arrays;
+
 public class MoveZeros {
     //2.Array -- Move Zeros to the End
     //Write a method that can move all the zeros to  last indexes of the array (Do Not Use Sort Method)
+
+    public static int[] moveZeroes(int[] array){
+
+        int[] newArr = new int [array.length];
+
+        int j = 0;
+        for (int i= 0; i < array.length; i++) {
+
+            if(array[i] != 0){
+                newArr[j++] = array[i];
+            }
+        }
+        return newArr;
+    }
+
+    public static int[] moveZeroesAsc(int[] arr){
+
+        for(int i = 0; i  < arr.length; i++){
+            for(int j = i+1; j < arr.length; j++){
+                if(arr[i] < arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+
+        int[] array = {2,0,5,0,1,7,0};
+        System.out.println(Arrays.toString(moveZeroes(array))); //keeping order
+        System.out.println();
+
+        System.out.println("Descending " + Arrays.toString(moveZeroesAsc(array)));
+    }
+
 
 }
--- src/day24_array/MoveZeros.java
+++ src/day24_array/MoveZeros.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class MoveZeros {
     public static void main(String[] args) {
 
-        int[] nums = {10, 0, 5, 0, 1, 0};
+        int[] nums = {10, 0, 5, 0,7, 1, 0};
         int[] fixed = new int[nums.length]; // [0, 0, 0, 0, 0, 0]
         int indexToRead = 0; //same as i in for loop
         int indexToStore = 0;
